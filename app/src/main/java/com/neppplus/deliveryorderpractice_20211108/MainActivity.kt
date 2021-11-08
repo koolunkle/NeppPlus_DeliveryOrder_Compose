8c3b7839edfecba075ea50a97f178692b8f4ea47
김준기 <gugi329@daum.net>
2021-11-08 22:39:36 +0900

    ViewPager 어댑터를 객체화 -> 화면의 ViewPager와 연결

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/MainActivity.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/MainActivity.kt
@@ -2,10 +2,27 @@ package com.neppplus.deliveryorderpractice_20211108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.deliveryorderpractice_20211108.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
+    }
+
 }
