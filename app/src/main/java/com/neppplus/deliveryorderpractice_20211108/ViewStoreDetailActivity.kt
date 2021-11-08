d99cd2489a2c0d2faa9bc5ecb407f192b1762701
김준기 <gugi329@daum.net>
2021-11-08 18:05:42 +0900

    상세보기 화면 추가

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/ViewStoreDetailActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.deliveryorderpractice_20211108
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
