35036a01a36ea67330e496e94b1364f9124cc7eb
김준기 <gugi329@daum.net>
2021-11-08 22:34:08 +0900

    임시 - 내 프로필 프래그먼트 초안 끝

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.deliveryorderpractice_20211108.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.deliveryorderpractice_20211108.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        동작 관련 코드 작성 예정
+
+    }
+
+
+}
