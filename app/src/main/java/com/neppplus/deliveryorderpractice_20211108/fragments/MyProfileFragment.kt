d727daef3170d2ef4262977960f2603051275263
김준기 <gugi329@daum.net>
2021-11-08 22:02:34 +0900

    닉네임 변경 누르면 -> 변경 화면으로 이동

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.neppplus.deliveryorderpractice_20211108.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.deliveryorderpractice_20211108.EditNicknameActivity
 import com.neppplus.deliveryorderpractice_20211108.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQ_CODE_NICKNAME = 1000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +26,14 @@ class MyProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 //        동작 관련 코드 작성 예정
+
+        btnEditNickname.setOnClickListener {
+
+            val myIntent = Intent(requireActivity(), EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQ_CODE_NICKNAME)
+
+        }
+
 
     }
 
