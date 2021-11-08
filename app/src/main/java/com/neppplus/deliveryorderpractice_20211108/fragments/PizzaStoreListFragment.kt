3480c7cec37f1070da95c9850c8e13c8e28173c7
김준기 <gugi329@daum.net>
2021-11-08 17:51:36 +0900

    피자가게 어댑터 객체화 / 리스트뷰와 연결  - requireContext()로 어느 화면인지 전달

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/PizzaStoreListFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.deliveryorderpractice_20211108.R
+import com.neppplus.deliveryorderpractice_20211108.adapters.PizzaStoreAdapter
 import com.neppplus.deliveryorderpractice_20211108.datas.StoreData
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<StoreData>()
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +32,10 @@ class PizzaStoreListFragment : Fragment() {
         mPizzaStoreList.add(  StoreData("파파존스", "1577-8080", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800")  )
         mPizzaStoreList.add(  StoreData("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200")  )
         mPizzaStoreList.add(  StoreData("도미노피자", "1577-3082", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png")  )
+
+
+        mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStoreList)
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
 
 
     }
