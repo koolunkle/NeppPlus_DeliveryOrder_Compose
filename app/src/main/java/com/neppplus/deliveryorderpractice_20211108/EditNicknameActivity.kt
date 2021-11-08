c7fb880fa0a593485cc8d75ef43f97970853edb8
김준기 <gugi329@daum.net>
2021-11-08 22:07:33 +0900

    확인 누르면 -> 입력한 닉네임 가지고 복귀

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.deliveryorderpractice_20211108
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,18 @@ class EditNicknameActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnOk.setOnClickListener {
+
+            val inputNickname = edtNewNickname.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+            setResult(RESULT_OK, resultIntent)
+
+            finish()
+
+        }
 
     }
 
