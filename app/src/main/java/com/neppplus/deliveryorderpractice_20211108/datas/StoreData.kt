d6bb6e3dd2935e2d05810a35941df227601b6475
김준기 <gugi329@daum.net>
2021-11-09 01:36:10 +0900

    [주석] 가게 데이터 표현용 클래스 추가

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/datas/StoreData.kt
@@ -0,0 +1,9 @@
+package com.neppplus.deliveryorder_20211108.datas
+
+class StoreData(
+    val name: String,
+    val phoneNum: String,
+    val logoURL: String ) {
+//  * 우리가 표현할 가게 이름(name) & 전화번호(phoneNum) & 로고(logoURL)를 표현하자!
+
+}
