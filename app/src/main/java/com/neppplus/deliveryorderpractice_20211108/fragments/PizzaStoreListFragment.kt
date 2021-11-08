88a4290ce44938564261fa9875cb1fc512b5ae4b
김준기 <gugi329@daum.net>
2021-11-09 02:33:27 +0900

    [주석] 피자가게 어댑터 객체화 / 리스트뷰와 연결  - requireContext()로 어느 화면인지 전달

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/PizzaStoreListFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.deliveryorder_20211108.adapters.PizzaStoreAdapter
 import com.neppplus.deliveryorder_20211108.datas.StoreData
 import com.neppplus.deliveryorderpractice_20211108.R
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<StoreData>()
 //  * 우리가 표현할 데이터를 mPizzaStoreList로 선언하였다.
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
+//  * 변수는 미리 선언하고 작업은 나중에 진행한다. 원하는 시점에 작업 진행 도모 & 오류 방지
     override fun onCreateView(
 //      * onCreateView : Fragment에 내가 사용할 View를 생성한다.
         inflater: LayoutInflater,
@@ -33,6 +37,12 @@ class PizzaStoreListFragment : Fragment() {
         mPizzaStoreList.add(  StoreData("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200")  )
         mPizzaStoreList.add(  StoreData("도미노피자", "1577-3082", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png")  )
 //      * 선언한 변수를 토대로 데이터 목록에 표현할 값을 입력하였다.
+
+        mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStoreList)
+//      * mPizzaStoreList 데이터를 store_list_item에 적용(연결)시킨다.
+//      * 적용(연결)시킬 화면에 맞는 코드를 입력하자! (this Vs. requireContext)
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
+//      * ListView에 데이터를 적용(연결)시키자
 
     }
 
