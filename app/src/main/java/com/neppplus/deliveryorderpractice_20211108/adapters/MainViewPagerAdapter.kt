3b38e0133d66b1fe6b6f788db4b5ca70680de8c2
김준기 <gugi329@daum.net>
2021-11-08 17:27:14 +0900

    ViewPager 어댑터 작업

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.neppplus.deliveryorderpractice_20211108.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.deliveryorderpractice_20211108.fragments.MyProfileFragment
+import com.neppplus.deliveryorderpractice_20211108.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter( fm: FragmentManager ) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+
+    }
+
+}
