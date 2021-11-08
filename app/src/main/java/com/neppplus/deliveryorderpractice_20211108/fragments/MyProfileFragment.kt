fae1cd2cf405997157a2424fb730cd44d10e5958
김준기 <gugi329@daum.net>
2021-11-09 00:41:48 +0900

    [주석] 임시 - 내 프로필 프래그먼트 초안 끝

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.neppplus.deliveryorder_20211108.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.deliveryorderpractice_20211108.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+//      * onCreateView : Fragment에 내가 사용할 View를 생성한다.
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+//      * fragment_my_profile xml 파일을 객체화하여 불러온다.
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//      * onActivityCreated : Fragment에 View가 생성되고 난후 이를 Activity에 적용한다.
+        super.onActivityCreated(savedInstanceState)
+
+//        동작 관련 코드 작성 예정
+
+    }
+
+
+}
