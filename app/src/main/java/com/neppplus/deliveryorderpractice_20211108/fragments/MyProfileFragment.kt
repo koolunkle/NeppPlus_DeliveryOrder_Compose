c8eabcb23c507ff088639989a884c316d412273b
김준기 <gugi329@daum.net>
2021-11-08 22:11:00 +0900

    돌아온 프래그먼트에서 닉네임 받아서 반영

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.deliveryorderpractice_20211108.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -34,6 +35,23 @@ class MyProfileFragment : Fragment() {
 
         }
 
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val newNickname = data!!.getStringExtra("nick")
+
+                txtNickname.text = newNickname
+
+            }
+
+        }
 
     }
 
