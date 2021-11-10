2b860b683e1523083dc2846f2183b402657d4b6c
김준기 <gugi329@daum.net>
2021-11-10 23:43:51 +0900

    [추가 요구사항] 로딩 화면 추가

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.deliveryorderpractice_20211108
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+}
