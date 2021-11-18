6de1ea9d9e7005ee3cbf24f17ec7dbdb9cef8fed
koolunkle <gugi329@daum.net>
2021-11-18 22:53:58 +0900

    [추가 요구사항] 치킨가게 어댑터 객체화 / 리스트뷰와 연결   - requireContext()로 어느 화면인지 전달

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/ChickenStoreListFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/ChickenStoreListFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.deliveryorderpractice_20211108.R
+import com.neppplus.deliveryorderpractice_20211108.adapters.ChickenStoreAdapter
 import com.neppplus.deliveryorderpractice_20211108.datas.StoreData
+import kotlinx.android.synthetic.main.fragment_chicken_store_list.*
 
 class ChickenStoreListFragment : Fragment() {
 
     val mChickenStoreList = ArrayList<StoreData>()
+    lateinit var mChickenStoreAdapter : ChickenStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +32,10 @@ class ChickenStoreListFragment : Fragment() {
         mChickenStoreList.add(  StoreData("네네치킨", "1599-4479", "https://t1.daumcdn.net/cfile/tistory/252B7248586EE8E017")  )
         mChickenStoreList.add(  StoreData("굽네치킨", "1688-9292", "https://www.shuttledelivery.co.kr/uploads/c787d35b08bfaf5cecd583a4307992ea.jpg")  )
         mChickenStoreList.add(  StoreData("페리카나", "1588-9292", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJTaNUsZVUd3xxYux8DQv5oxkQOtaWB1TdRi8VMOidfbU5XpvziP4WXjmuJo-DMlF2pl4&usqp=CAU")  )
+
+
+        mChickenStoreAdapter = ChickenStoreAdapter(requireContext(), R.layout.store_list_item, mChickenStoreList)
+        chickenStoreListView.adapter = mChickenStoreAdapter
 
 
     }
