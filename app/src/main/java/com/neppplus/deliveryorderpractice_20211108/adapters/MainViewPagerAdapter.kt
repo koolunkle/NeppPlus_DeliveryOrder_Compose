bfb8051a0c9fe821b84e64ab73392384f307427e
koolunkle <gugi329@daum.net>
2021-11-18 22:39:41 +0900

    [추가 요구사항] ViewPager 어댑터 작업

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/adapters/MainViewPagerAdapter.kt
@@ -3,16 +3,18 @@ package com.neppplus.deliveryorderpractice_20211108.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.deliveryorderpractice_20211108.fragments.ChickenStoreListFragment
 import com.neppplus.deliveryorderpractice_20211108.fragments.MyProfileFragment
 import com.neppplus.deliveryorderpractice_20211108.fragments.PizzaStoreListFragment
 
-class MainViewPagerAdapter( fm: FragmentManager ) : FragmentPagerAdapter(fm) {
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
-        return when(position) {
+        return when (position) {
 
             0 -> "피자 주문"
+            1 -> "치킨 주문"
             else -> "내 정보 설정"
 
         }
@@ -20,13 +22,14 @@ class MainViewPagerAdapter( fm: FragmentManager ) : FragmentPagerAdapter(fm) {
     }
 
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 
     override fun getItem(position: Int): Fragment {
 
-        return when(position) {
+        return when (position) {
             0 -> PizzaStoreListFragment()
+            1 -> ChickenStoreListFragment()
             else -> MyProfileFragment()
         }
 
