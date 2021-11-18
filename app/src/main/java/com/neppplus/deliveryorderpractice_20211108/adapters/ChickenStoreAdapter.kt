4249b169a838b65586fe125be64fdbc06432b3e1
koolunkle <gugi329@daum.net>
2021-11-18 22:45:32 +0900

    [추가 요구사항] 치킨가게 어댑터 기초 작업 중

---
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/adapters/ChickenStoreAdapter.kt
@@ -0,0 +1,12 @@
+package com.neppplus.deliveryorderpractice_20211108.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.neppplus.deliveryorderpractice_20211108.datas.StoreData
+
+class ChickenStoreAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StoreData>
+) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+}
