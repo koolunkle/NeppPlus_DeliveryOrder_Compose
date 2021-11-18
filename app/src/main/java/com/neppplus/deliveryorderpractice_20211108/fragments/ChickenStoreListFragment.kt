60fbab9a8c48bcc4f64147602f192c3b5e2b23d3
koolunkle <gugi329@daum.net>
2021-11-18 22:51:20 +0900

    [추가 요구사항] 프래그먼트에서, 가게 데이터 4개 추가

--- app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/ChickenStoreListFragment.kt
+++ app/src/main/java/com/neppplus/deliveryorderpractice_20211108/fragments/ChickenStoreListFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.deliveryorderpractice_20211108.R
+import com.neppplus.deliveryorderpractice_20211108.datas.StoreData
 
 class ChickenStoreListFragment : Fragment() {
+
+    val mChickenStoreList = ArrayList<StoreData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +25,13 @@ class ChickenStoreListFragment : Fragment() {
 
 //        동작 관련 코드 작성 예정
 
+        mChickenStoreList.add(  StoreData("BBQ", "1588-9282", "https://mblogthumb-phinf.pstatic.net/20160507_267/ppanppane_1462548783710FjRMn_PNG/Untitled-1-34.png?type=w800")  )
+        mChickenStoreList.add(  StoreData("네네치킨", "1599-4479", "https://t1.daumcdn.net/cfile/tistory/252B7248586EE8E017")  )
+        mChickenStoreList.add(  StoreData("굽네치킨", "1688-9292", "https://www.shuttledelivery.co.kr/uploads/c787d35b08bfaf5cecd583a4307992ea.jpg")  )
+        mChickenStoreList.add(  StoreData("페리카나", "1588-9292", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJTaNUsZVUd3xxYux8DQv5oxkQOtaWB1TdRi8VMOidfbU5XpvziP4WXjmuJo-DMlF2pl4&usqp=CAU")  )
+
+
     }
+
 
 }
